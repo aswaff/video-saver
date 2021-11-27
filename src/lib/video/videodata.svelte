087dc43cad545966ca3eeef386342ec5569dd593
unknown <averyds17@gmail.com>
2021-11-27 18:30:17 -0500

    First Commit

---
+++ src/lib/video/videodata.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+export const Videodata = {
+    "id":1,
+    "url": "https://v16-web.tiktok.com/video/tos/useast2a/tos-useast2a-ve-0068c002/5af9e03ad24b497f902c5933ffadcf21/?a=1988&br=2330&bt=1165&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&expire=1638067764&ft=wUyFfFGgkag3-I&l=202111272048370101881701700E6E1DCE&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&policy=3&qs=0&rc=M3JscDc6ZjV3NzMzNzczM0ApaWdmZjxnOzw3NzQzOWU4Nmdza2wucjRnY3NgLS1kMTZzc14vMS9iYjQ0MzEyMjMxMzM6Yw%3D%3D&signature=58bfcd4baee557dd467f264095a5baab&tk=0&vl=&vr=",
+}  
+
+</script>
+
+<!-- "url": "https://v16-web.tiktok.com/video/tos/useast2a/tos-useast2a-ve-0068c002/5af9e03ad24b497f902c5933ffadcf21/?a=1988&br=2330&bt=1165&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&expire=1638067764&ft=wUyFfFGgkag3-I&l=202111272048370101881701700E6E1DCE&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&policy=3&qs=0&rc=M3JscDc6ZjV3NzMzNzczM0ApaWdmZjxnOzw3NzQzOWU4Nmdza2wucjRnY3NgLS1kMTZzc14vMS9iYjQ0MzEyMjMxMzM6Yw%3D%3D&signature=58bfcd4baee557dd467f264095a5baab&tk=0&vl=&vr=", -->
---
+++ src/routes/index.svelte
@@ -0,0 +1,60 @@
+<!-- <script context="module">
+	export const prerender = true;
+</script> -->
+
+<script>
+	import {Videodata} from "../lib/video/videodata.svelte";
+	
+</script>
+
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+<!-- <video
+src="">
+<track kind="captions">
+</video> -->
+<div class="iframe">
+	<iframe
+		class="iframeplaya"
+		title="video"
+		src={Videodata.url}>
+	</iframe>
+</div>
+<section>
+
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	.iframe {
+		align-self: center;
+	}
+
+	.iframeplaya {
+		height: 80vh;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
