4c2c538855cfcc13607d228ace263ee8cd0e7a65
unknown <averyds17@gmail.com>
2021-12-19 20:42:01 -0500

    Posting properly to database, with all data needed

--- src/routes/add.svelte
+++ src/routes/add.svelte
@@ -1,13 +1,31 @@
 <script	>
 import { onMount } from "svelte";
 import { ApiData } from "$lib/video/store.js"
-let URL = ""
+let URLbox = ""
 let Catagory = ""
 let iframeResult;
 let iframelyPromise;
 let response;
 let databaseURLs;
 let URLnew = null;
+
+let categoryOptions = [
+	{ id: "1", value: "Cute"},
+	{ id: "2", value: "Music"},
+	{ id: "3", value: "Other"}
+]
+
+// Post data returned
+let POSTresult = null;
+
+// Data for POST
+let URL = null;
+let URLembed = null;
+let HTML = null;
+let Thumbnail = null;
+let Category = "Other";
+
+
 // const json = await res.json()
 		
 // 		iframeResult = {"URL": json.URL,
@@ -15,6 +33,7 @@ let URLnew = null;
 // 						"Thumbnail": json.test.links.thumbnail[0].href}
 // 		console.log(json)
 
+// Pulls videos from database
 onMount(async () => {
   fetch("http://192.168.0.16:5000/tasks")
   .then(response => response.json())
@@ -26,7 +45,7 @@ onMount(async () => {
     return [];
   });
 });
-
+// checks to see if url already exists in database
 const checkURL = (url) => {
 	const databaseURLs = $ApiData.map(data => data.URL)
 	if (databaseURLs.includes(url) === true) {
@@ -41,9 +60,9 @@ const checkURL = (url) => {
 }
 
 
-
+// function from submit URL button to go to iframely
 const iframeGet = async () => {
-		const res = await fetch(`https://iframe.ly/api/iframely?url=${URL}&key=5684147d970b5663f90cfc1c15410b81`)
+		const res = await fetch(`https://iframe.ly/api/iframely?url=${URLbox}&key=5684147d970b5663f90cfc1c15410b81`)
 		
 		if (res.status === 200){
 			return res.json()
@@ -57,17 +76,41 @@ const iframeGet = async () => {
 
 
 
-
+// first button to look up a URL with iframely
 const firstClick = () => {
 	iframelyPromise = iframeGet();
 		const iframePromiseResponse = async () => {
 			const a = await iframelyPromise;
+			iframeResult = a
+			URL = a.url
+			URLembed = a.links.player[0].href
+			HTML = a.links.player[0].html
+			Thumbnail = a.links.thumbnail[0].href
+			console.log(a)
 			checkURL(a.url);
 		}
 		iframePromiseResponse()
 }
-
-
+// Performs the push to the mongo database
+async function doPost () {
+		const res = await fetch('http://192.168.0.16:5000/tasks', {
+			method: 'POST',
+			headers: {
+      			"Content-Type": "application/json",
+    		},
+			body: JSON.stringify({
+				URL,
+				URLembed,
+				HTML,
+				Thumbnail,
+				Category
+			})
+		})
+		
+		const json = await res.json()
+		POSTresult = res.status
+		
+	}
 
 </script>
 
@@ -80,9 +123,9 @@ const firstClick = () => {
 	
 	<form class="content">
 		<label>Video URL</label>
-		<input type="text"  bind:value={URL}/>
+		<input type="text"  bind:value={URLbox}/>
 		<button type="button" on:click={firstClick}>
-			Post it.
+			Search for Video
 		</button>
 	  </form>
 	  {#await iframelyPromise}
@@ -93,9 +136,19 @@ const firstClick = () => {
 	  {#if URLnew === null}
 	  <p></p>
 	 	 {:else if URLnew}
-		<button>
+		  {#if POSTresult === null}
+		  <select bind:value={Catagory} on:change="{() => Catagory = ''}">
+			{#each categoryOptions as option}
+				<option value={option}>{option.value}</option>
+			{/each}	
+		</select>
+		<button type="button" on:click={doPost}>
 			Submit Video
 		</button>
+		{:else}
+		<p>Video has been submitted</p>
+		{/if}
+
 		{:else}
 		<p>Video Already exists in database</p>
 		
@@ -113,12 +166,14 @@ const firstClick = () => {
 		width: 100%;
 		max-width: var(--column-width);
 		margin: var(--column-margin-top) auto 0 auto;
+		text-align: center;
 	}
 
 	img {
-		width: 70vw;
+		height: 25vh;
 		display: flex;
 		margin-left: auto;
+		margin-right: auto;
 	}
 
 </style>
